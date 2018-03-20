0e527feb3ed31d114501c9864d565aa21837b3e3
Unknown <marc-antoine.therrien@polymtl.ca>
2018-03-20 17:26:07 -0400

    vorace

--- tp2/vorace/bloque.py
+++ tp2/vorace/bloque.py
@@ -1,16 +1,24 @@
 class Bloque:
-    longueur = 0
+    profondeur = 0
     largeur = 0
     hauteur = 0
+    surface = 0
+    ratio = 0
 
-    def makeBloque(longueur, largeur, hauteur):
-        bloc = Bloque()
-        bloc.longueur = longueur
-        bloc.largeur = largeur
-        bloc.hauteur = hauteur
-        print(bloc.longueur)
-        return bloc
+    def __init__(self, info):
+        self.hauteur = int(info[0])
+        self.largeur = int(info[1])
+        self.profondeur = int(info[2])
+        self.surface = self.largeur * self.profondeur
+        self.ratio = self.surface / self.hauteur
 
-    def test(self):
-        print("test")
+    def getLongueur(self):
+        return self.longueur
 
+    # def makeBloque(longueur, largeur, hauteur):
+    #    bloc = Bloque()
+    ##   bloc.longueur = longueur
+    #   bloc.largeur = largeur
+    ##  bloc.hauteur = hauteur
+    #  print(bloc.longueur)
+    #  return bloc
--- tp2/vorace/vorace.py
+++ tp2/vorace/vorace.py
@@ -1,30 +1,57 @@
-import sys
 import os
 from bloque import Bloque
+from random import randint
+
+
+candidatNum = 60
+solution = []
+hauteurSolution = 0
+
+
+def glouton(candidat):
+    i = 0
+    global hauteurSolution
+    while i < len(candidat):
+        if len(solution) == 0:
+            solution.append(candidat[0])
+            hauteurSolution = hauteurSolution + candidat[0].hauteur
+        else:
+            #trouver le plus a gauche qui rentre
+            if solution[(len(solution)-1)].profondeur > candidat[i].profondeur and solution[(len(solution)-1)].largeur > candidat[i].largeur:
+                solution.append(candidat[i])
+                hauteurSolution = hauteurSolution + candidat[i].hauteur
+            i = i+1
 
 
 def vorace(fileToRead):
-    print(fileToRead)
+    fileBlocks = open("../data/" + fileToRead)
+    blocksArray = []
+    line = fileBlocks.readline()
+    while line:
+        blockInfo = line.split()
+        newBlock = Bloque(blockInfo)
+        blocksArray.append(newBlock)
+        line = fileBlocks.readline()
+    fileBlocks.close()
+    blocksCandidats = []
+    while len(blocksArray) > 0:
+        for i in range(0, candidatNum):
+            if len(blocksArray) == 0:
+                print("")
+            else:
+                x = randint(0, (len(blocksArray)-1))
+                blocksCandidats.append(blocksArray[x])
+                del blocksArray[x]
+        blocksCandidats.sort(key=lambda x: x.ratio, reverse=False);
+        glouton(blocksCandidats)
+        blocksCandidats.clear()
 
 
 if __name__ == '__main__':
     txtFiles = os.listdir("../data")
-    i = 12
     for file in txtFiles:
         vorace(file)
-        if i == 12:
-            temp = open("../data/"+file)
-            i = i+1
-
-    bloc = Bloque.makeBloque(temp.readline(), 12, 12)
-    print(bloc.largeur)
-
-
-#block = Block(12, 22, 33)
-#print block.longueur
-#print block.hauteur
-#print block.largeur
-
-#F = open("../data/b100_1.txt")
-#print F.readline()
-
+        print(hauteurSolution)
+        hauteurSolution = 0
+        solution.clear()
+        break
